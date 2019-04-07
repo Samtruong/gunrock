c917854a4b6894f79c0833606de12e793f655945
samtruong <mstruong@ucdavis.edu>
2019-04-06 22:59:55 -0700

    add dataset color run

---
+++ tests/color/frogRun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+path="/data-2/mstruong/Frog_dataset/"
+addr0=${path}"youtube.mtx"
+addr1=${path}"roadNetCA.mtx"
+addr2=${path}"wikiTalk.mtx"
+addr3=${path}"amazon.mtx"
+addr4=${path}"dblp.mtx"
+addr5=${path}"twitter.mtx"
+ADDR_ARRAY=($addr0 $addr1 $addr2 $addr3 $addr4)
+
+
+	echo "[0] Run youtube dataset"
+	echo "[1] Run roadNetCA dataset"
+	echo "[2] Run wikiTalk dataset"
+	echo "[3] Run amazon dataset"
+	echo "[4] Run dblp dataset"
+	echo "[5] Run twitter dataset"
+	read -p "Option: " set
+	./bin/test_color_10.0_x86_64 \
+	--graph-type=market \
+	--graph-file=${ADDR_ARRAY[$set]} \
+	--JPL=true \
+	--no-conflict=0 \
+	--user-iter=0 \
+	--prohibit-size=0 \
+	--quick=false \
+	--device=0 \
+	--min-color=true \
+	--test-run=true \
+	--undirected \
+	--remove-duplicate-edges \
+	--remove-self-loops 
